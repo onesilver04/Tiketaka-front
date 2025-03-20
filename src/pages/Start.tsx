d88a158ff34739ff37b5aaecb88fe1a9d6dd851e
한은정 <141193305+onesilver04@users.noreply.github.com>
2025-03-20 19:49:23 +0900

    feat:Start Page

---
+++ src/pages/Start.tsx
@@ -0,0 +1,17 @@
+import logoMain from "../assets/lolgo_main.svg";
+import styles from "../styles/Button.module.css";
+import "../styles/Start.css";
+
+const Start = () => {
+    return (
+        <div>
+            <div className="logo">
+                <img src={logoMain} alt="main-logo" />
+            </div>
+            <button className={`${styles.button} reservation`}>예매하기</button>
+            <button className={`${styles.button} look-up`}>조회하기</button>
+        </div>
+    );
+};
+
+export default Start;
