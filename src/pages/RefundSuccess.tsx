948dc3a6ed209f06d9dcfb296d4991f055d64da2
한은정 <141193305+onesilver04@users.noreply.github.com>
2025-03-26 15:28:02 +0900

    feat: refund-success 완료

--- src/pages/RefundSuccess.tsx
+++ src/pages/RefundSuccess.tsx
@@ -1,7 +1,15 @@
+import "../styles/RefundSuccess.css";
+
 const RefundSuccess = () => {
     return (
-        <div>
+        <div className="refund-success">
+            <div className="ment">
+                <p>환불 처리 완료되었습니다.</p>
+                <p>Tiketaka를 이용해주셔서 감사합니다.</p>
+            </div>
+            <img src="/src/assets/success-button.svg" alt="success-img" />
         </div>
     );
-}
+};
+
 export default RefundSuccess;
